f8dd51653658e93ac788d539fb39b3dd43dda2b7
tqvuong-99 <tranquocvuong2102elec@gmail.com>
2025-02-22 17:38:00 +0700

    create route

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,18 @@
 import express, {Request, Response}  from 'express';
-
+import categoriesRouter from './routes/v1/categories.route';
 
 
 const app = express();
-const PORT = 3000;
+
+
+// -------------||BEGIN REGISTER ROUTES ||----------------
+
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World');
 });
+// Đăng kí route từ file bên ngoài
+app.use('/api/v1',categoriesRouter);
+// -------------||END REGISTER ROUTES ||----------------
 
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
-});
-
-
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,9 @@
-asfasfagsav dsdiskldjkld
+import app from './app';
+import {env} from "./helpers/env.helper"
+const PORT = env.PORT;
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+  });
+  
