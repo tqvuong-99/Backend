edb1c9c1557c8fc48d5740fcdadf15da814a3b99
tqvuong-99 <tranquocvuong2102elec@gmail.com>
2025-02-22 18:31:56 +0700

    handle

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
-import express, {Request, Response}  from 'express';
+import express, {Request, Response, NextFunction}  from 'express';
 import categoriesRouter from './routes/v1/categories.route';
+import createError from 'http-errors';
 
 
 const app = express();
@@ -14,5 +15,19 @@ app.get('/', (req: Request, res: Response) => {
 // Đăng kí route từ file bên ngoài
 app.use('/api/v1',categoriesRouter);
 // -------------||END REGISTER ROUTES ||----------------
+// -------------||BEGIN HANDLE ERRORS ||----------------
+  // catch 404 and forward to error handler
+  app.use(function(req: Request, res: Response,next: NextFunction){
+    next(createError(404));
+  });
+app.use(function (err: any, req: Request, res: Response, next: NextFunction) {
+  // set locals, only providing error in development
+  const statusCode = err.status || 500;
+  res.status(statusCode).json({ 
+    statusCode: statusCode,
+    message: err.message 
+  });
+});
+// -------------||END HANDLE ERRORS ||----------------
 
 export default app;
--- src/routes/v1/categories.route.ts
+++ src/routes/v1/categories.route.ts
@@ -1,10 +1,28 @@
 import express from 'express';
-
+import createError from 'http-errors';
 const router = express.Router();
+const categories = [
+    { id: 1, name: 'Category 1' },
+    { id: 2, name: 'Category 2' },
+    { id: 3, name: 'Category 3' },
+    { id: 4, name: 'Category 4' },
+     ];
+// GET All categories
 // GET/api/v1/categories
 router.get('/categories', (req, res) => {
-    res.send("categories");
+    res.status(200).json(categories);
+
 });
-
-
+// GET Category by ID
+router.get('/categories/:id', (req, res) => {
+    const { id } = req.params;
+    // Tìm category theo id truyền vào
+    const category = categories.find(category => category.id == Number(id));
+    // Nếu không tìm thấy thì hiển thị "Category not found"
+    if (!category) {
+        // throw new Error('Category not found');
+        throw createError(400, 'Category not found');
+    }
+    res.status(200).json(category);
+});
 export default router;
